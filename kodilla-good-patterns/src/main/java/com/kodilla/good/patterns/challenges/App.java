1eaa8d06c6eb736decd34a6f1179c5fe324eb9db
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-10-03 22:58:11 +0200

    Napisałem rozwiązanie na trzy sposoby dwia pierwsze są fajne, problem z nimi polega na tym, że nie działają, mógłbyś rzucić okiem? W MovieStore pierwsza zakomentowana metoda pokazuje, że zwraca void; chociaż powinien być string, a druga ma jakiś problem z collect.

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/App.java
@@ -0,0 +1,8 @@
+package com.kodilla.good.patterns.challenges;
+
+public class App {
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        movieStore.getMovieTitles();
+    }
+}
---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -0,0 +1,64 @@
+package com.kodilla.good.patterns.challenges;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MovieStore {
+
+    public Map<String, List<String>> getMovies() {
+
+        List<String> ironManTranslations = new ArrayList<>();
+        ironManTranslations.add("Żelazny Człowiek");
+        ironManTranslations.add("Iron Man");
+
+        List<String> avengersTranslations = new ArrayList<>();
+        avengersTranslations.add("Mściciele");
+        avengersTranslations.add("Avengers");
+
+        List<String> flashTranslations = new ArrayList<>();
+        flashTranslations.add("Błyskawica");
+        flashTranslations.add("Flash");
+
+        Map<String, List<String>> booksTitlesWithTranslations = new HashMap<>();
+        booksTitlesWithTranslations.put("IM", ironManTranslations);
+        booksTitlesWithTranslations.put("AV", avengersTranslations);
+        booksTitlesWithTranslations.put("FL", flashTranslations);
+
+        return booksTitlesWithTranslations;
+    }
+
+//    public String getMovieTitles() {
+//
+//                String result = getMovies()
+//                .entrySet()
+//                .stream()
+//                .map(Map.Entry::getValue)
+//                .collect(Collectors.joining("!"));
+//        return result;
+//    }
+
+
+//    public String getMovieTitles() {
+//
+//        String result = getMovies()
+//                .forEach((s, strings) -> {
+//                    strings
+//                            .stream()
+//                            .collect(Collectors.joining("!"));
+//                });
+//        return result;
+//    }
+
+
+    public String getMovieTitles() {
+
+        getMovies().forEach((s, strings) -> {
+            strings
+                    .stream()
+                    .map(s1 -> s1 + "!")
+                    .forEach(System.out::println);
+        });
+
+        return "";
+    }
+}
