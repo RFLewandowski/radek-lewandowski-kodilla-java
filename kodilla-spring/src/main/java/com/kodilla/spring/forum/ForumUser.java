bcdef212ded98d61679e57d7b6e6ba2911fd420d
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-10-14 14:44:41 +0200

    task 10.1 corrected

--- kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -1,10 +1,10 @@
 package com.kodilla.spring.forum;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@lombok.Value
+@Value
 public class ForumUser {
     private String username;
 
--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -4,21 +4,21 @@ package com.kodilla.spring.forum;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ForumUserTestSuite {
 
+    @Autowired
+    private ForumUser forumUser;
+
     @Test
     public void Should_getUserName() {
         //Given
-        ApplicationContext context =
-                new AnnotationConfigApplicationContext("com.kodilla.spring");
-        ForumUser forumUser = context.getBean(ForumUser.class);
+        //
         //When
         String actualName = forumUser.getUsername();
         //Then
