6a729d3055f64fc61a35e6b1d48c3c5b6f265efc
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-10-18 23:43:54 +0200

    10.3 still no progress - failed to load application context

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -4,38 +4,37 @@ package com.kodilla.spring.portfolio;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class BoardConfig {
 
+
     private final TaskList toDoList;
     private final TaskList inProgressList;
     private final TaskList doneList;
+    private final Board board;
 
-    public BoardConfig(@Qualifier("toDoList") TaskList toDoList, @Qualifier("inProgressList") TaskList inProgressList, @Qualifier("doneList") TaskList doneList) {
+    public BoardConfig(@Qualifier("toDoList") TaskList toDoList, @Qualifier("inProgressList") TaskList inProgressList, @Qualifier("doneList") TaskList doneList, @Qualifier("board") Board board) {
         this.toDoList = toDoList;
         this.inProgressList = inProgressList;
         this.doneList = doneList;
+        this.board = board;
     }
 
     @Bean(name = "toDoList")
     @Qualifier("toDoList")
-    @Scope("prototype")
     public TaskList getToDoList() {
         return toDoList;
     }
 
     @Bean(name = "inProgressList")
     @Qualifier("inProgressList")
-    @Scope("prototype")
     public TaskList getInProgressList() {
         return inProgressList;
     }
 
     @Bean(name = "doneList")
     @Qualifier("doneList")
-    @Scope("prototype")
     public TaskList getDoneList() {
         return doneList;
     }
