4813c7dc7dee80b6499051f8062f5dc12eae90c9
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-10-18 22:30:16 +0200

    11.2 ready

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Library.java
@@ -1,21 +1,41 @@
 package com.kodilla.patterns.prototype.library;
 
+import com.kodilla.patterns.prototype.Prototype;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.HashSet;
 import java.util.Set;
 
-public final class Library {
-    final String name;
-    final Set<Book> books = new HashSet<>();
+@Setter
+@Getter
+public final class Library extends Prototype {
+    String name;
+    Set<Book> books = new HashSet<>();
 
-    public Library(final String name) {
+    public Library(String name) {
         this.name = name;
     }
 
-    public String getName() {
-        return name;
+    public Library shallowCopy() throws CloneNotSupportedException {
+        return (Library) super.clone();
     }
 
-    public Set<Book> getBooks() {
-        return books;
+    public Library deepCopy() throws CloneNotSupportedException {
+        Library clonedLibrary = (Library) super.clone();
+
+        clonedLibrary.books = new HashSet<>();
+        for (Book book : books) {
+            Book clonedBook = new Book(
+                    book.getTitle(),
+                    book.getAuthor(),
+                    book.getPublicationDate());
+
+            clonedLibrary
+                    .getBooks()
+                    .add(clonedBook);
+        }
+        return clonedLibrary;
     }
+
 }
--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -1,11 +1,58 @@
 package com.kodilla.patterns.prototype.library;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.stream.IntStream;
+
 public class LibraryTestSuite {
+    Library originalLibrary = new Library("originalLibrary");
 
+    @Test
+    public void Should_makeShallowCopy() {
+        //Given
+        Library shallowCopiedLibrary = null;
 
+        try {
+            shallowCopiedLibrary = originalLibrary.shallowCopy();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+        //When
+        shallowCopiedLibrary.getBooks().add(new Book("t", "a", LocalDate.now()));
+        //Then
+        Assert.assertEquals(shallowCopiedLibrary.getBooks(), originalLibrary.getBooks());
+    }
 
+    @Test
+    public void Should_makeDeepCopy() {
+        //Given
+        Library deepCopiedLibrary = null;
 
-    public void Should_testGetBooks(){
+        try {
+            deepCopiedLibrary = originalLibrary.deepCopy();
+        } catch (CloneNotSupportedException e) {
+            e.printStackTrace();
+        }
+
+        //When
+        deepCopiedLibrary.getBooks().add(new Book("t", "a", LocalDate.now()));
+
+        //Then
+        Assert.assertEquals(10, originalLibrary.getBooks().size());
+        Assert.assertEquals(11, deepCopiedLibrary.getBooks().size());
+    }
+
+    @Before
+    public void fillInLibrary() {
+        IntStream
+                .iterate(1, n -> n + 1)
+                .limit(10)
+                .forEach(n -> originalLibrary
+                        .getBooks()
+                        .add(new Book("title " + n, "author " + n, LocalDate.now())));
 
     }
 }
