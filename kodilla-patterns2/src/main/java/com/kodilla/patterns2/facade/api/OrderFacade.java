b8711333ab501508b06b594826fd19ea66b25509
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-12-23 15:35:23 +0100

    facade refactored

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/api/OrderFacade.java
@@ -17,46 +17,52 @@ public class OrderFacade {
         this.shopService = shopService;
     }
 
+    private boolean wasError = false;
+
     public void processOrder(final OrderDto order, final Long userId) throws OrderProcessingException {
-        boolean wasError = false;
         long orderId = shopService.openOrder(userId);
         log.info("Registering new order, ID: " + orderId);
-        if (orderId < 0) {
-            log.error(OrderProcessingException.ERR_NOT_AUTHORISED);
-            wasError = true;
-            throw new OrderProcessingException(OrderProcessingException.ERR_NOT_AUTHORISED);
-        }
+        validate(orderId < 0, OrderProcessingException.ERR_NOT_AUTHORISED);
+
         try {
-            for (ItemDto orderItem : order.getItems()) {
-                log.info("Adding item " + orderItem.getProductId() + ", " +
-                        orderItem.getQuantity() + " pcs");
-                shopService.addItem(orderId, orderItem.getProductId(), orderItem.getQuantity());
-            }
+            addItemsToOrder(orderId, order);
             BigDecimal value = shopService.calculateValue(orderId);
             log.info("Order valuse is: " + value + " USD");
-            if (!shopService.doPayment(orderId)) {
-                log.error(OrderProcessingException.ERR_PAYMENT_REJECTED);
-                wasError = true;
-                throw new OrderProcessingException(OrderProcessingException.ERR_PAYMENT_REJECTED);
-            }
+
+            validate(shopService.verifyOrder(orderId), OrderProcessingException.ERR_PAYMENT_REJECTED);
             log.info("Payment for order was done");
-            if (!shopService.verifyOrder(orderId)) {
-                log.error(OrderProcessingException.ERR_VERIFICATION_ERROR);
-                wasError = true;
-                throw new OrderProcessingException(OrderProcessingException.ERR_VERIFICATION_ERROR);
-            }
+
+            validate(shopService.verifyOrder(orderId), OrderProcessingException.ERR_VERIFICATION_ERROR);
             log.info("Order is ready to submit");
-            if (!shopService.submitOrder(orderId)) {
-                log.error(OrderProcessingException.ERR_SUBMITTING_ERROR);
-                wasError = true;
-                throw new OrderProcessingException(OrderProcessingException.ERR_SUBMITTING_ERROR);
-            }
+
+            validate(shopService.submitOrder(orderId), OrderProcessingException.ERR_SUBMITTING_ERROR);
             log.info("Order " + orderId + " submitted");
+
         } finally {
             if (wasError) {
                 log.info("Cancelling order " + orderId);
                 shopService.cancelOrder(orderId);
             }
         }
+    }
+
+    private void validate(boolean isProcessOk, String errorType) throws OrderProcessingException {
+        if (!isProcessOk) {
+            manageProcessingException(errorType);
+        }
+    }
+
+    private void addItemsToOrder(long orderId, OrderDto order) {
+        for (ItemDto orderItem : order.getItems()) {
+            log.info("Adding item " + orderItem.getProductId() + ", " +
+                    orderItem.getQuantity() + " pcs");
+            shopService.addItem(orderId, orderItem.getProductId(), orderItem.getQuantity());
+        }
+    }
+
+    private void manageProcessingException(String errorType) throws OrderProcessingException {
+        log.error(errorType);
+        wasError = true;
+        throw new OrderProcessingException(errorType);
     }
 }
