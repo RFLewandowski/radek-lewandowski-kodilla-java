1d453e0b797b75d4062258d09f88e33151c88f6f
RFLewandowski <R.F.Lewandowski@gmail.com>
2017-12-29 13:04:09 +0100

    added test for UpdateBestsellers MySql procedure

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredPostTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredPostTestSuite.java
@@ -10,24 +10,46 @@ public class StoredPostTestSuite {
 
     @Test
     public void Should_UpdateVipLevels() throws Exception {
+        //Given&When&Then
+        int howMany = updateTester(
+                "READERS",
+                "VIP_LEVEL",
+                "UpdateVipLevels");
+
+        Assert.assertEquals(0, howMany);
+    }
+
+    @Test
+    public void Should_UpdateBestsellers() throws Exception {
+        //Given&When&Then
+        int howMany = updateTester(
+                "BOOKS",
+                "BESTSELLER",
+                "UpdateBestsellers");
+
+        Assert.assertEquals(0, howMany);
+    }
+
+    private int updateTester(String testedTable, String testedColumn, String updateProcedure) throws Exception {
         //Given
         DbManager dbManager = DbManager.getInstance();
-        String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"NOT SET\"";
+        String sqlUpdate = "UPDATE " + testedTable + " SET " + testedColumn + "=NULL";
         Statement statement = dbManager.getConnection()
                 .createStatement();
         statement.execute(sqlUpdate);
 
         //When
-        String sqlProcedureCall = "CALL UpdateVipLevels()";
+        String sqlProcedureCall = "CALL " + updateProcedure + "()";
         statement.execute(sqlProcedureCall);
 
         //Then
-        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"NOT SET\"";
+        String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM " + testedTable + " WHERE " + testedColumn + "=NULL";
         ResultSet rs = statement.executeQuery(sqlCheckTable);
         int howMany = -1;
-        if(rs.next()){
-            howMany=rs.getInt("HOW_MANY");
+        if (rs.next()) {
+            howMany = rs.getInt("HOW_MANY");
         }
-        Assert.assertEquals(0,howMany);
+
+        return howMany;
     }
 }
