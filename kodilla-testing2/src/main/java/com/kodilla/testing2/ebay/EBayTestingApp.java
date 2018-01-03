1cf8cb8fbd32470ae10e14f020293504998a349c
RFLewandowski <R.F.Lewandowski@gmail.com>
2018-01-03 23:39:21 +0100

    added gradle file

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -1,4 +1,21 @@
 package com.kodilla.testing2.ebay;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class EBayTestingApp {
+
+    public static final String SEARCHFIELD = "gh-ac";
+
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.ebay.com");
+
+        WebElement searchfield = driver.findElement(By.id(SEARCHFIELD));
+        searchfield.sendKeys("Laptop");
+        searchfield.submit();
+    }
 }
--- kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -1,4 +1,20 @@
 package com.kodilla.testing2.google;
 
+import com.kodilla.testing2.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
 public class GoogleTestingApp {
+
+    public static final String SEARCHFIELD = "lst-ib";
+
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+        driver.get("https://www.google.com");
+
+        WebElement searchfield = driver.findElement(By.id(SEARCHFIELD));
+        searchfield.sendKeys("Kodilla");
+        searchfield.submit();
+    }
 }
